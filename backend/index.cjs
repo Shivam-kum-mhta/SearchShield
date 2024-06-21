5ed5049f84c2f213fb5a2947acf09d363837bd6c
[firstname lastname] <sk8625228@gmail.com>
2024-06-21 21:36:01 +0530

    login in successful, history in pending

--- backend/index.cjs
+++ backend/index.cjs
@@ -31,17 +31,27 @@ app.get('/api/context', async (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
-  
   try {
     const newUser = new User({ username, email, password });
     await newUser.save();
     res.status(201).json({ message: 'User registered successfully' });
-    console.log(`${username} is registered`)
+    console.log(`${username, email, password} is registered`)
   } catch (error) {
     res.status(400).json({ error: 'Error registering user' });
   }
 });
 
+app.post('/history', async (req, res)=>{
+  const {keywords}=req.body;
+  try{
+    const newSearchHistory = new SearchHistory({keywords});
+    await newSearchHistory.save();
+    res.status(200).json({ message: 'Search history successfully' });
+    console.log(`keywords =${keywords}`)
+  } catch (error) {
+    res.status(402).json({error: 'Error creating search history'})}
+  }
+)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
